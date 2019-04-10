6c3b24b4060881d4be6b949a24386041dd86587f
Liyue1998 <1757759700@qq.com>
2019-04-10 16:10:22 +0800

    uglify分支

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,22 @@
-module.exports = function (grunt) {
+module.exports = function(grunt){
   grunt.initConfig({
-    mochacli: {
-      options: {
-                reporter: 'spec',
-          bail: true
-          
-      },
-        all: ['test/*.js']
-                         
-    },
-    mocha_istanbul: {
-    coverage: {
-            src: 'test'
-                  
+    uglify: {
+      release: {
+        files: {
+          'dist/rectangle.min.js': ['./rectangle.js'],
+          'dist/calc.min.js':['./calc.js']      
+            
+        }
+                                                 
+      }                                         
+                   
     }
-        
-    },
-    istanbul_check_coverage: {
-    default: {
-    options: {
-              coverageFolder: 'coverage*',
-              check: {
-                          lines: 90,
-                                      statements: 90
-                                                
-              }
-                      
-    }
-          
-    }
-        
-    }
-      
+                          
+              
   });
 
-    grunt.loadNpmTasks('grunt-mocha-cli');
-      grunt.loadNpmTasks('grunt-mocha-istanbul');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default',['uglify']);
 
-        grunt.registerTask('default', ['mochacli']);   
-          grunt.registerTask('cover', ['mocha_istanbul']);
-            grunt.registerTask('check-cover', ['istanbul_check_coverage']);
 
 };
-
